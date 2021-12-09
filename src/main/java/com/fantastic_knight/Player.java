b227bba7ae2739d872e21eb7cd146a0a6ec0a3c7
Simon2523 <simongraber2002@gmail.com>
2021-12-09 15:16:31 +0100

    sautv3

--- src/main/java/com/fantastic_knight/Player.java
+++ src/main/java/com/fantastic_knight/Player.java
@@ -27,6 +27,7 @@ public class Player extends Sprite {
     Image image;
     double vSaut = 18;
     int lastMove;
+    double lastX = xPosition;
 
     // 0 RIGHT; 180 LEFT
 
@@ -37,7 +38,7 @@ public class Player extends Sprite {
         shape = new Rectangle(width,height,Color.BLACK);
         xPosition = 25;
         yPosition = 25;
-        xVelocity = 2;
+        xVelocity = 5;
         yVelocity = 0;
         angle = 0;
         shape.setX(xPosition);
@@ -46,19 +47,23 @@ public class Player extends Sprite {
     }
 
     public void moveLeft() {
-        xVelocity = 2;
+        if(state==State.JUMP || state==State.FALL) return;
+        xVelocity = 5;
         yVelocity = 0;
         angle = 180;
         lastMove = 1;
         state = State.WALK;
+        System.out.println("etat walkL");
     }
 
     public void moveRight() {
-        xVelocity = 2;
+        if(state==State.JUMP || state==State.FALL) return;
+        xVelocity = 5;
         yVelocity = 0;
         angle = 0;
         lastMove = 2;
         state = State.WALK;
+        System.out.println("etat walkR");
     }
 
     public void stop() {
@@ -92,131 +97,143 @@ public class Player extends Sprite {
     }
 
     public void jump() {
-        if(state==State.JUMP) return;
+        if(state==State.JUMP || state==State.IDLE) return;
         if(lastMove==1) angle=180;
         else if(lastMove==2) angle=0;
-        xVelocity=2;
+        xVelocity=5;
         yVelocity = - vSaut;
         state = State.JUMP;
+        System.out.println("etat jump");
     }
 
     @Override
     public void update() {
 
-        double x;
-        double y;
+        double x=0;
+        double y=0;
 
+        Rectangle shape1 = new Rectangle(width,height,Color.BLACK);
         if (state == State.IDLE) {
-            return; // no movement
+            return;
         } else { // moving or falling
 
-            // Create a copie
-            Rectangle shape1 = new Rectangle(width,height,Color.BLACK);
-
-            if(state==State.JUMP){
-                y = yPosition + yVelocity;
-                shape1.setX(xPosition);
-                shape1.setY(y);
-                boolean isFloor = false;
-                for(Shape s : model.obstacles) {
-                    Shape inter = Shape.intersect(shape1,s);
-                    Bounds b = inter.getBoundsInLocal();
-                    if (b.getWidth() != -1) {
-                        isFloor = true;
-                    }
-                }
-                if(isFloor){
-                    state = State.FALL;
-                    angle = 90;
-                }
-                if (!isFloor) {
-                    yPosition += yVelocity;
-                }
-            }
-
-            if (state == State.WALK) {
-                // NO FLOOR DETECTION for actual position: just move perso one pixel down
-                shape1.setX(xPosition);
-                shape1.setY(yPosition+1);
-                boolean isFloor = false;
-                for(Shape s : model.obstacles) {
-                    Shape inter = Shape.intersect(shape1,s);
-                    Bounds b = inter.getBoundsInLocal();
-                    if (b.getWidth() != -1) {
-                        isFloor = true;
-                    }
-                }
-
-                // No floor => change angle & state to falling
-                if (!isFloor) {
-                    state = State.FALL;
-                    angle = 90;
-                }
-            }
+            testJump(shape1,y);
+            testWalk(shape1);
 
             if (state == State.FALL || state == State.JUMP){
-                yVelocity ++;
+                if(yPosition>=model.height) state = State.IDLE;
+                else yVelocity ++;
             }
 
-            x = getNewX();
-            y = getNewY();
+            testCollision(shape1,x,y);
+        }
+    }
 
-            // COLLISION DETECTION
-            shape1.setX(x);
+    public void testJump(Rectangle shape1,double y){
+        if(state==State.JUMP){
+            y = yPosition + yVelocity;
+            shape1.setX(xPosition);
             shape1.setY(y);
-
-            // Which obstacle in collision with Player
-            List<Shape> lst = new ArrayList<>();
+            boolean isFloor = false;
             for(Shape s : model.obstacles) {
                 Shape inter = Shape.intersect(shape1,s);
                 Bounds b = inter.getBoundsInLocal();
                 if (b.getWidth() != -1) {
-                    lst.add(s);
+                    isFloor = true;
+                }
+            }
+            if(isFloor){
+                state = State.FALL;
+                System.out.println("etat fall");
+                angle = 90;
+            }
+            if (!isFloor) {
+                yPosition += yVelocity;
+            }
+        }
+    }
+
+    public void testWalk(Rectangle shape1){
+        if (state == State.WALK) {
+            // NO FLOOR DETECTION for actual position: just move perso one pixel down
+            shape1.setX(xPosition);
+            shape1.setY(yPosition+1);
+            boolean isFloor = false;
+            for(Shape s : model.obstacles) {
+                Shape inter = Shape.intersect(shape1,s);
+                Bounds b = inter.getBoundsInLocal();
+                if (b.getWidth() != -1) {
+                    isFloor = true;
                 }
             }
 
-            // list not empty => collision
-            // search for the minimal move to get a collision
-            if (!lst.isEmpty()) {
-                for(int i=1;i<=xVelocity;i++) {
-                    xVelocity = i;
-                    yVelocity = i;
-                    x = getNewX();
-                    y = getNewY();
-                    shape1.setX(x);
-                    shape1.setY(y);
-                    boolean collide = false;
-                    for(Shape s : lst) {
-                        Shape inter = Shape.intersect(shape1,s);
-                        Bounds b = inter.getBoundsInParent();
-                        if (b.getWidth() != -1) {
-                            collide = true;
-                        }
-                    }
+            // No floor => change angle & state to falling
+            if (!isFloor) {
+                state = State.FALL;
+                System.out.println("etat fall");
+                angle = 90;
+            }
+        }
+    }
 
-                    if (collide) {
-                        state = State.IDLE; // stop moving
-                        xVelocity = i-1;
-                        yVelocity = i-1;
-                        x = getNewX();
-                        y = getNewY();
-                        xVelocity = 0;
-                        yVelocity = 0;
-                        break;
-                    }
-                }
-                setxVelocity(xVelocity);
-                setyVelocity(yVelocity);
+    public void testCollision(Rectangle shape1, double x, double y){
+        x = getNewX();
+        y = getNewY();
+
+        // COLLISION DETECTION
+        shape1.setX(x);
+        shape1.setY(y);
+
+        // Which obstacle in collision with Player
+        List<Shape> lst = new ArrayList<>();
+        for(Shape s : model.obstacles) {
+            Shape inter = Shape.intersect(shape1,s);
+            Bounds b = inter.getBoundsInLocal();
+            if (b.getWidth() != -1) {
+                lst.add(s);
             }
         }
 
+        // list not empty => collision
+        // search for the minimal move to get a collision
+        if (!lst.isEmpty()) {
+            for(int i=1;i<=xVelocity;i++) {
+                xVelocity = i;
+                yVelocity = i;
+                x = getNewX();
+                y = getNewY();
+                shape1.setX(x);
+                shape1.setY(y);
+                boolean collide = false;
+                for(Shape s : lst) {
+                    Shape inter = Shape.intersect(shape1,s);
+                    Bounds b = inter.getBoundsInParent();
+                    if (b.getWidth() != -1) {
+                        collide = true;
+                    }
+                }
+
+                if (collide) {
+                    state = State.IDLE; // stop moving
+                    System.out.println("etat idle");
+                    xVelocity = i-1;
+                    yVelocity = i-1;
+                    x = getNewX();
+                    y = getNewY();
+                    xVelocity = 0;
+                    yVelocity = 0;
+                    break;
+                }
+            }
+            setxVelocity(xVelocity);
+            setyVelocity(yVelocity);
+        }
         // RENDER
         setxPosition(x);
         setyPosition(y);
         shape.setX(getxPosition());
         shape.setY(getyPosition());
     }
-
 
     public Rectangle getShape() {
         return shape;
