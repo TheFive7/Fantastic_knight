6c76ced8b2875cfcd305b3a3fc9d480caf26fd92
TheFive7 <hennequin.maxime2412@gmail.com>
2022-01-08 11:07:09 +0100

    * code commenté     * chevalier regarde maintenant aussi à gauche     * animation du chevalier à gauche     * Arrangement de parties du code     * Quelques bugs réglés

--- src/main/java/com/fantastic_knight/controller/LevelsMenu.java
+++ src/main/java/com/fantastic_knight/controller/LevelsMenu.java
@@ -5,35 +5,47 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Rectangle;
 
 import static com.fantastic_knight.Game.*;
 import static com.fantastic_knight.Game.primaryStage;
 
-public class LevelsController extends Pane {
+public class LevelsMenu extends Pane {
 
-    public LevelsController(){
+    public LevelsMenu(){
+        // Taille
         setHeight(800);
         setWidth(1200);
+
+        // Background
         ImageView bgImage = new ImageView(new Image("file:src/main/resources/com/fantastic_knight/levels.png"));
+
+        // Bouton pour retourner au menu
         Button buttonMenu = new Button();
         buttonMenu.setLayoutX(1128); buttonMenu.setLayoutY(697); buttonMenu.setOnAction(e -> returnMenu());
         buttonMenu.setOpacity(0); buttonMenu.setPrefSize(60,85.3);
+
         getChildren().addAll(bgImage, buttonMenu);
 
+        // Parcours des niveaux
         int i = 0; int j = 0;
         int compteur = 1;
         for (String levelName : levels) {
+            // Affichage et création
             ButtonLevel buttonLevel = new ButtonLevel(levelName,60 + j * 250, 219 + i * 130);
             int finalCompteur = compteur;
             buttonLevel.setOnAction(e -> selectLevel(finalCompteur));
             getChildren().add(buttonLevel);
-            compteur++;
-            i++;
+            compteur++; i++;
+
+            // Si 4 niveaux dans une colonne, on commence une nouvelle colonne
             if (i == 4){i = 0; j++;}
         }
     }
 
+    /**
+     * Selectionner un niveau
+     * @param i : Numero du niveau
+     */
     public void selectLevel(int i){
         if (model.state == Model.STATE_INITIAL) {
             primaryStage.setScene(scene);
