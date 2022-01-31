bd6b4bfd0a4b79286da5df464c5a7a110d17abfd
TheFive7 <hennequin.maxime2412@gmail.com>
2022-01-31 20:28:08 +0100

    * Réagencement de code * Porte de sortie créée * Porte de sortie ajoutée au LevelMaker * Crédits faits * Ajustements des niveaux * Bug avec le bouclier du personnage réglé

--- src/main/java/com/fantastic_knight/model/Door.java
+++ src/main/java/com/fantastic_knight/model/Door.java
@@ -1,19 +1,24 @@
 package com.fantastic_knight.model;
 
 import javafx.geometry.Bounds;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
 public class Door extends Rectangle {
-    final double width = 50;
+    final double width = 70;
     final double height = 100;
     final Model model;
+    Image image;
 
     public Door(Model model) {
         super();
         this.model = model;
         setWidth(width);
         setHeight(height);
+        image = new Image("file:src/main/resources/com/fantastic_knight/door.png");
+        setFill(new ImagePattern(image));
     }
 
     public void update() {
