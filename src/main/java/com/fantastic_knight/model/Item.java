402c967a1b9d10de90a448cf0a96681347ea5bad
TheFive7 <hennequin.maxime2412@gmail.com>
2022-01-31 20:28:14 +0100

    * Réagencement de code * Porte de sortie créée * Porte de sortie ajoutée au LevelMaker * Crédits faits * Ajustements des niveaux * Bug avec le bouclier du personnage réglé

--- src/main/java/com/fantastic_knight/model/Item.java
+++ src/main/java/com/fantastic_knight/model/Item.java
@@ -1,6 +1,5 @@
 package com.fantastic_knight.model;
 
-import com.fantastic_knight.Sprite;
 import javafx.scene.image.Image;
 import javafx.scene.shape.Rectangle;
 
@@ -19,68 +18,8 @@ abstract class Item extends Sprite {
         super(model);
     }
 
-    public Rectangle getShape() {
-        return shape;
-    }
-
     public void setShape(Rectangle shape) {
         this.shape = shape;
-    }
-
-    public String getEffect() {
-        return effect;
-    }
-
-    public void setEffect(String effect) {
-        this.effect = effect;
-    }
-
-    public double getxPosition() {
-        return xPosition;
-    }
-
-    public void setxPosition(double xPosition) {
-        this.xPosition = xPosition;
-    }
-
-    public double getyPosition() {
-        return yPosition;
-    }
-
-    public void setyPosition(double yPosition) {
-        this.yPosition = yPosition;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public boolean isAnimated() {
-        return animated;
-    }
-
-    public void setAnimated(boolean animated) {
-        this.animated = animated;
     }
 
     public Image getImage() {
