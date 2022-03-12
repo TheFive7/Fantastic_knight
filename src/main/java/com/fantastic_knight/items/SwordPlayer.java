decd374d4fcadbe2a486ebaf3a46a15b94df996e
TheFive7 <hennequin.maxime2412@gmail.com>
2022-03-12 16:52:36 +0100

    * Multijoueur implémenté

--- src/main/java/com/fantastic_knight/items/SwordPlayer.java
+++ src/main/java/com/fantastic_knight/items/SwordPlayer.java
@@ -1,6 +1,7 @@
 package com.fantastic_knight.items;
 
 import com.fantastic_knight.model.Model;
+import com.fantastic_knight.player.Player;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
@@ -11,9 +12,11 @@ public class SwordPlayer extends Item {
     Image imageLeft = new Image("file:src/main/resources/com/fantastic_knight/items/sword_left.png");
     ImagePattern imagePatternRight = new ImagePattern(imageRight);
     ImagePattern imagePatternLeft = new ImagePattern(imageLeft);
+    Player player;
 
-    public SwordPlayer(Model model) {
+    public SwordPlayer(Model model, Player player) {
         super(model);
+        this.player = player;
         type = "sword";
         width = 70;
         height = 70;
@@ -28,11 +31,11 @@ public class SwordPlayer extends Item {
     @Override
     public void update() {
         if (isActive) {
-            getShape().setY(model.player.getyPosition());
-            if (model.player.getAngle() == 0) {
-                getShape().setX(model.player.getxPosition() + 10);
-            } else if (model.player.getAngle() == 180) {
-                getShape().setX(model.player.getxPosition() - 40);
+            getShape().setY(player.getyPosition());
+            if (player.getAngle() == 0) {
+                getShape().setX(player.getxPosition() + 10);
+            } else if (player.getAngle() == 180) {
+                getShape().setX(player.getxPosition() - 40);
             }
         }
     }
@@ -42,14 +45,14 @@ public class SwordPlayer extends Item {
      * @param direction : Direction of the sword
      */
     public void orientationSword(String direction){
-        getShape().setY(model.player.getyPosition());
+        getShape().setY(player.getyPosition());
         if (getActive()){
             if (direction.equals("right")) {
                 getShape().setFill(imagePatternRight);
-                getShape().setX(model.player.getxPosition() + 10);
+                getShape().setX(player.getxPosition() + 10);
             } else {
                 getShape().setFill(imagePatternLeft);
-                getShape().setX(model.player.getxPosition() - 40);
+                getShape().setX(player.getxPosition() - 40);
             }
         } else {
             getShape().setFill(new ImagePattern(new Image("file:src/main/resources/com/fantastic_knight/assets/anything.png")));
