5dec711ae3739a9504e5357881ef0288529b4e78
TheFive7 <hennequin.maxime2412@gmail.com>
2022-03-12 16:51:41 +0100

    * Multijoueur implémenté

--- src/main/java/com/fantastic_knight/items/Door.java
+++ src/main/java/com/fantastic_knight/items/Door.java
@@ -1,7 +1,7 @@
 package com.fantastic_knight.items;
 
-import com.fantastic_knight.items.Item;
 import com.fantastic_knight.model.Model;
+import com.fantastic_knight.player.Player;
 import javafx.geometry.Bounds;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
@@ -25,17 +25,19 @@ public class Door extends Item {
     }
 
     public void update() {
-        double x = model.player.getxPosition();
-        double y = model.player.getyPosition();
-        Rectangle joueur = new Rectangle(model.player.getWidth(), model.player.getHeight());
-        joueur.setX(x);
-        joueur.setY(y);
+        for (Player player : model.sprites) {
+            double x = player.getxPosition();
+            double y = player.getyPosition();
+            Rectangle joueur = new Rectangle(player.getWidth(), player.getHeight());
+            joueur.setX(x);
+            joueur.setY(y);
 
-        Shape inter = Shape.intersect(joueur, shape);
-        Bounds b = inter.getBoundsInParent();
-        if (b.getWidth() != -1) {
-            if (!model.player.isWin() && model.isKey) {
-                model.player.setWin(true);
+            Shape inter = Shape.intersect(joueur, shape);
+            Bounds b = inter.getBoundsInParent();
+            if (b.getWidth() != -1) {
+                if (!player.isWin() && model.isKey) {
+                    player.setWin(true);
+                }
             }
         }
     }
