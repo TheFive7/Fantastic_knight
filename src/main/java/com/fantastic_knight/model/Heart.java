3154ef0ac40d92a9c8b3f98f8b27738d33433b33
TheFive7 <hennequin.maxime2412@gmail.com>
2022-03-12 16:51:52 +0100

    * Multijoueur implémenté

--- src/main/java/com/fantastic_knight/model/Heart.java
+++ src/main/java/com/fantastic_knight/model/Heart.java
@@ -1,6 +1,7 @@
 package com.fantastic_knight.model;
 
 import com.fantastic_knight.items.Item;
+import com.fantastic_knight.player.Player;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
@@ -8,10 +9,12 @@ import javafx.scene.shape.Rectangle;
 public class Heart extends Item {
 
     Model model;
+    Player player;
 
-    public Heart(Model model) {
+    public Heart(Model model, Player player) {
         super(model);
         this.model = model;
+        this.player = player;
         setShape(new Rectangle(32, 32));
         getShape().setX(0);
         getShape().setY(0);
@@ -27,8 +30,8 @@ public class Heart extends Item {
             Thread t = new Thread(timer);
 
             // Display Heart
-            getShape().setX(model.player.getShape().getX() - 32);
-            getShape().setY(model.player.getShape().getY() - 32);
+            getShape().setX(player.getShape().getX() - 32);
+            getShape().setY(player.getShape().getY() - 32);
             getShape().setFill(new ImagePattern(new Image("file:src/main/resources/com/fantastic_knight/assets/heart_burst.png")));
             t.start();
         } else {
