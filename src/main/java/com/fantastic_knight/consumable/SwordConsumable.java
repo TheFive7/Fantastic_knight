32f63853bacebd14ca857b1c529105c15268fa81
TheFive7 <hennequin.maxime2412@gmail.com>
2022-03-12 16:52:33 +0100

    * Multijoueur implémenté

--- src/main/java/com/fantastic_knight/consumable/SwordConsumable.java
+++ src/main/java/com/fantastic_knight/consumable/SwordConsumable.java
@@ -1,7 +1,7 @@
 package com.fantastic_knight.consumable;
 
 import com.fantastic_knight.model.Model;
-import com.fantastic_knight.model.Sword;
+import com.fantastic_knight.player.Player;
 import javafx.geometry.Bounds;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
@@ -24,17 +24,19 @@ public class SwordConsumable extends Consumable {
 
     @Override
     public void update() {
-        playerHitbox.setX(model.player.getxPosition());
-        playerHitbox.setY(model.player.getyPosition());
+        for (Player player : model.sprites) {
+            playerHitbox.setX(player.getxPosition());
+            playerHitbox.setY(player.getyPosition());
 
-        if (isActive){
-            Shape inter = Shape.intersect(playerHitbox, shape);
-            Bounds b = inter.getBoundsInLocal();
-            if (b.getWidth() != -1) {
-                isActive = false;
-                model.sword.setFill(new ImagePattern(new Image("file:src/main/resources/com/fantastic_knight/consumables/sword.png")));
-                getShape().setFill(new ImagePattern(new Image("file:src/main/resources/com/fantastic_knight/assets/anything.png")));
-                model.player.setSword(true);
+            if (isActive) {
+                Shape inter = Shape.intersect(playerHitbox, shape);
+                Bounds b = inter.getBoundsInLocal();
+                if (b.getWidth() != -1) {
+                    isActive = false;
+                    player.getSword().setFill(new ImagePattern(new Image("file:src/main/resources/com/fantastic_knight/consumables/sword.png")));
+                    getShape().setFill(new ImagePattern(new Image("file:src/main/resources/com/fantastic_knight/assets/anything.png")));
+                    player.setSword(true);
+                }
             }
         }
     }
