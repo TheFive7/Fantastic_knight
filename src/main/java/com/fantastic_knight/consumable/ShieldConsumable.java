3a884dda99c580c416817ca5806c26226cb4cbf2
TheFive7 <hennequin.maxime2412@gmail.com>
2022-03-12 16:52:23 +0100

    * Multijoueur implémenté

--- src/main/java/com/fantastic_knight/consumable/ShieldConsumable.java
+++ src/main/java/com/fantastic_knight/consumable/ShieldConsumable.java
@@ -1,6 +1,7 @@
 package com.fantastic_knight.consumable;
 
 import com.fantastic_knight.model.Model;
+import com.fantastic_knight.player.Player;
 import javafx.geometry.Bounds;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
@@ -23,17 +24,19 @@ public class ShieldConsumable extends Consumable {
 
     @Override
     public void update() {
-        playerHitbox.setX(model.player.getxPosition());
-        playerHitbox.setY(model.player.getyPosition());
+        for (Player player : model.sprites) {
+            playerHitbox.setX(player.getxPosition());
+            playerHitbox.setY(player.getyPosition());
 
-        if (isActive){
-            Shape inter = Shape.intersect(playerHitbox, shape);
-            Bounds b = inter.getBoundsInLocal();
-            if (!model.player.isLife()) {
-                if (b.getWidth() != -1) {
-                    isActive = false;
-                    model.player.setLife(true);
-                    getShape().setFill(new ImagePattern(new Image("file:src/main/resources/com/fantastic_knight/assets/anything.png")));
+            if (isActive) {
+                Shape inter = Shape.intersect(playerHitbox, shape);
+                Bounds b = inter.getBoundsInLocal();
+                if (!player.isLife()) {
+                    if (b.getWidth() != -1) {
+                        isActive = false;
+                        player.setLife(true);
+                        getShape().setFill(new ImagePattern(new Image("file:src/main/resources/com/fantastic_knight/assets/anything.png")));
+                    }
                 }
             }
         }
