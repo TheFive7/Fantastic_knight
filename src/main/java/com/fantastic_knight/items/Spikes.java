7419cd1afa335da9772bb300ae69fe98865d6085
TheFive7 <hennequin.maxime2412@gmail.com>
2022-03-13 02:18:18 +0100

    * Ajouts des chevaliers dans les options * Correctifs multijoueur * Ajout d'un ennemi (en test) * Bug sur l'épée * Possibilité de tuer l'ennemi * L'ennemi fait de simples rondes

--- src/main/java/com/fantastic_knight/items/Spikes.java
+++ src/main/java/com/fantastic_knight/items/Spikes.java
@@ -29,7 +29,7 @@ public class Spikes extends Item {
      */
     @Override
     public void update() {
-        for (Player player : model.sprites) {
+        for (Player player : model.players) {
             playerHitbox.setX(player.getxPosition());
             playerHitbox.setY(player.getyPosition());
 
