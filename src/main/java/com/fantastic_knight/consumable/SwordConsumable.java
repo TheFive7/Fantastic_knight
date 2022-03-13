638ee14c6d5f2dfe517bfc55df64a1fc250ecbfe
TheFive7 <hennequin.maxime2412@gmail.com>
2022-03-13 02:18:23 +0100

    * Ajouts des chevaliers dans les options * Correctifs multijoueur * Ajout d'un ennemi (en test) * Bug sur l'épée * Possibilité de tuer l'ennemi * L'ennemi fait de simples rondes

--- src/main/java/com/fantastic_knight/consumable/SwordConsumable.java
+++ src/main/java/com/fantastic_knight/consumable/SwordConsumable.java
@@ -24,7 +24,7 @@ public class SwordConsumable extends Consumable {
 
     @Override
     public void update() {
-        for (Player player : model.sprites) {
+        for (Player player : model.players) {
             playerHitbox.setX(player.getxPosition());
             playerHitbox.setY(player.getyPosition());
 
